fece019b844cb8b651722be8f8a95e4c93467f49
GEORGE HOLMES <JAZZYMCSUPERFLY@GMAIL.COM>
2016-05-11 15:43:15 -0700

    adds question 6 to app.js too high too low

--- app.js
+++ app.js
@@ -58,3 +58,33 @@ if (response5 === answer5 || response5 === answer5[0]) {
   alert('How wrong you are, ' + userName + '. I guess you will never realize just how important JavaScript programming really is.');
   console.log('wrong answer5');
 }
+
+// Question 6 "Too high/low w/ 4 chances and alert of remaining chances"
+
+var chances = 4;
+var answer6 = 4;
+
+while (chances > 0) {
+  var response6 = parseInt(prompt('Ok, ' + userName + ', here is a tough one. How many years have I been married? (YOU CURRENTLY HAVE ' + chances + ' TRIES TO GUESS CORRECTLY - HINT: THE ANSWER IS MORE THAN ZERO AND LESS THAN TEN)'));
+  console.log(response6);
+  if (response6 === answer6) {
+    alert('You got it right, ' + userName + '! George has been married to his wife, Kelly, for 4 magical years.');
+    console.log('The user guessed correctly');
+    chances = 0;
+  } else if ((chances > 0) && (response6 < answer6)) {
+    chances--;
+    alert('Sorry, ' + userName + ', too low. Guess again! You currently have ' + chances + ' remaining.');
+    console.log('The user guessed too low');
+  } else if ((chances > 0) && (response6 > answer6)) {
+    chances--;
+    alert('Sorry, ' + userName + ', too high. Guess again! You currently have ' + chances + ' remaining.');
+    console.log('The user guessed too high');
+  } else if (chances === 0) {
+    alert('Looks like you ran out of guesses, ' + userName + '! Try again next time, ok?');
+    console.log('The user ran out of guesses');
+  } else {
+    chances--;
+    alert('Sorry, ' + userName + ', no sense in wasting your guesses. Should have entered a number! You now have ' + chances + ' remaining.');
+    console.log('The user\'s entry was NaN');
+  }
+}
